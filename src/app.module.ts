96e5aeb28774158a9115f59f1b96c02380a742e9
C-1412 <cesartabaresespinosa@gmail.com>
2025-11-16 12:53:50 -0500

    backend nest user control jwt guard

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'users_control',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
